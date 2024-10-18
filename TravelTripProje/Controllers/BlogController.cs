7884c3eff05889bd57aa5d17b5eb0e9f99807f4b
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-10-18 23:23:10 +0300

    edits added

---
+++ TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+using TravelTripProje.Models.Classes;
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        context c = new context();
+        public ActionResult Index()
+        {
+            var bloglar = c.Blogs.ToList();
+            return View(bloglar);
+        }
+        public ActionResult BlogDetay(int id)
+        {
+            return View();
+        }
+    }
+}
