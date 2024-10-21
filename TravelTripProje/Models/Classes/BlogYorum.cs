d1d1634db9e0449cf7fd95ef20913d9e681caa49
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-10-21 21:35:23 +0300

    BlogDetay page edit and BlogYorum Page added

---
+++ TravelTripProje/Models/Classes/BlogYorum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TravelTripProje.Models.Classes;
+
+namespace TravelTripProje.Models.Classes
+{
+    public class BlogYorum
+    {
+        public IEnumerable<blog>Deger1 { get; set; }
+        public IEnumerable<comment>Deger2 { get; set; }
+
+    }
+}
