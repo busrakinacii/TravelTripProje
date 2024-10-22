19a31e87cf0face2189778a3521f7f899d74bde3
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-10-22 22:07:14 +0300

    Partial1 and Partial2 added

--- TravelTripProje/Controllers/DefaultController.cs
+++ TravelTripProje/Controllers/DefaultController.cs
@@ -3,19 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Classes;
 
 namespace TravelTripProje.Controllers
 {
     public class DefaultController : Controller
     {
         // GET: Default
+        context c = new context();
         public ActionResult Index()
         {
-            return View();
+
+            var deger = c.Blogs.ToList();
+            return View(deger);
         }
         public ActionResult About()
         {
             return View();
+        }
+        public PartialViewResult Partial1()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
+            return PartialView(degerler);
+        }
+
+        public PartialViewResult Partial2()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.ID==1).ToList();
+            return PartialView(degerler);
         }
     }
 }
