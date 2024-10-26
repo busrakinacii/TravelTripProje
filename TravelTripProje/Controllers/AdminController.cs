55c39567c0e3f537ec6a01ba06e7bc14671a7a0f
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-10-26 21:58:53 +0300

    Changes saved

--- TravelTripProje/Controllers/AdminController.cs
+++ TravelTripProje/Controllers/AdminController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelTripProje.Models.Classes;
 
 namespace TravelTripProje.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin
+        context c = new context();
         public ActionResult Index()
         {
-            return View();
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
         }
     }
 }
