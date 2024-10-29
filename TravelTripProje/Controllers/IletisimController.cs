d6036cd81a647be392cc1fc05fdea710c93c023e
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-10-29 21:45:23 +0300

    Mesaj Page Added and edited

---
+++ TravelTripProje/Controllers/IletisimController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Classes;
+
+namespace TravelTripProje.Controllers
+{
+    public class IletisimController : Controller
+    {
+        // GET: Iletisim
+        context c = new context();
+        public ActionResult Index()
+        {
+            var degerler = c.AddressBlogs.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public PartialViewResult Mesaj()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult Mesaj(communication com)
+        {
+            c.Communications.Add(com);
+            c.SaveChanges();
+            return PartialView();
+        }
+
+    }
+}
