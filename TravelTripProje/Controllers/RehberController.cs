e049093e2cd79a301d0e42e0f25c1cd4c43bda74
Büşra Kınacı <75859913+busrakinacii@users.noreply.github.com>
2024-10-30 13:34:38 +0300

    rehbercontroller added

---
+++ TravelTripProje/Controllers/RehberController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Classes;
+
+namespace TravelTripProje.Controllers
+{
+    public class RehberController : Controller
+    {
+        // GET: Rehber
+        context c = new context();
+        public ActionResult Index()
+        {
+            var sirala = c.Blogs.Take(10).ToList();
+            return View(sirala);
+        }
+        public PartialViewResult Gorsel()
+        {
+            var g = c.Blogs.OrderByDescending(x => x.ID).Take(4).ToList();
+            return PartialView(g);
+        }
+
+    }
+}
